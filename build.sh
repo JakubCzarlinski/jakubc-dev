bbbbb874dbcd52e0762c95770581cfc48e30ff27
Jakub Czarlinski <jakubczarlinski@gmail.com>
2024-10-14 19:07:23 +0100

    Attempt to configure for deployment.

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-go install github.com/a-h/templ/cmd/templ@latest
-
-bun install
-go -C ./build/render_to_templ/ build -ldflags="-s -w" -o ./main.exe ./cmd/main.go
-go -C ./build/builder/ build -ldflags="-s -w" -o ./build.exe ./build.go
+go install github.com/a-h/templ/cmd/templ@latest &&
+bun install &&
+go -C ./build/render_to_templ/ build -ldflags="-s -w" -o ./main.exe ./cmd/main.go &&
+go -C ./build/builder/ build -ldflags="-s -w" -o ./build.exe ./build.go &&
 ./build/builder/build.exe
